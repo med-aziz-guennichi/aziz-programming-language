a70bc9f622bf22d60056defa354f1737eadea0b0
Med Aziz Guennichi <medazizguennichi@gmail.com>
2024-01-27 07:03:40 -0800

    exit the loop when he didint enter a value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::fs;
-use std::io::BufRead;
+use std::io::{self, BufRead, Write};
 use std::process::exit;
-use std::io;
 
 fn run_file(path: &str) -> Result<(), String> {
     match fs::read_to_string(path) {
@@ -14,17 +13,31 @@ fn run_file(path: &str) -> Result<(), String> {
 fn run(contents: &str) -> Result<(), String> {
     return Err("Not implemented".to_string());
 }
-fn run_prompt() -> Result<(), String>{
-  print!("> ");
-  let mut buffer: String = String::new();
-  let stdin: io::Stdin = io::stdin();
-  let mut handle: io::StdinLock<'_> = stdin.lock();
-  match handle.read_line(&mut buffer) {
-    Ok(_) => (),
-    Err(_) => return Err("Couldnt read line".to_string()),
-  }
-  println!("You wrote: {}", buffer);
-  Ok(())
+fn run_prompt() -> Result<(), String> {
+    while true {
+        print!("> ");
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => return Err("Couldnt not flush stdout".to_string()),
+        }
+
+        let mut buffer: String = String::new();
+        let stdin: io::Stdin = io::stdin();
+        let mut handle: io::StdinLock<'_> = stdin.lock();
+        match handle.read_line(&mut buffer) {
+            Ok(n) => {
+                if n <= 2 {
+                    println!("Bye!");
+                    return Ok(());
+                }
+            }
+            Err(_) => return Err("Couldnt read line".to_string()),
+        }
+
+        println!("You wrote: {}", buffer);
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -42,11 +55,11 @@ fn main() {
         }
     } else {
         match run_prompt() {
-          Ok(_) => exit(0),
-          Err(msg) => {
-            println!("Error:\n{}", msg);
-            exit(1);
-          }
+            Ok(_) => exit(0),
+            Err(msg) => {
+                println!("Error:\n{}", msg);
+                exit(1);
+            }
         }
     }
 }
